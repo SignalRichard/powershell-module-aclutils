eaef53a1930a0d361fafda3faa4eb0483d74b4e2
Richard Bolhofer <20459042+SignalRichard@users.noreply.github.com>
2023-02-14 12:15:15 -0500

    build: make sbom-tool executable; install node and npm packages in release job

--- build/scripts/Install-MicrosoftSbomTool.ps1
+++ build/scripts/Install-MicrosoftSbomTool.ps1
@@ -7,6 +7,11 @@ param(
 
 $LatestRelease = Invoke-RestMethod -Uri 'https://api.github.com/repos/microsoft/sbom-tool/releases/latest' -Method 'Get'
 $Url = ($LatestRelease.assets | Where-Object { $_.name -match "sbom-tool$OSArch" } | Select-Object -First 1).browser_download_url
-Invoke-WebRequest -Uri $Url -OutFile (Join-Path -Path $InstallPath -ChildPath "sbom-tool$(if($OSArch -match 'win') { '.exe' } else { '' })")
+$InstallFilePath = (Join-Path -Path $InstallPath -ChildPath "sbom-tool$(if($OSArch -match 'win') { '.exe' } else { '' })")
+Invoke-WebRequest -Uri $Url -OutFile $InstallFilePath
+
+if(-not ($OSArch -match 'win')) {
+    chmod +x $InstallFilePath
+}
 
 Write-Output $InstallPath >> $env:GITHUB_PATH
