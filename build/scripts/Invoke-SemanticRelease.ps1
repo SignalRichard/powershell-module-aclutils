52f02cd3d9399a9cd2d3e90d06726adafa3b324f
Richard Bolhofer <20459042+SignalRichard@users.noreply.github.com>
2023-02-14 13:29:02 -0500

    ci: setup main release

--- build/scripts/Invoke-SemanticRelease.ps1
+++ build/scripts/Invoke-SemanticRelease.ps1
@@ -2,11 +2,16 @@ param(
     [switch] $DryRun
 )
 
-if($DryRun) {
-    npx semantic-release --dry-run
+if($env:GH_REF -eq 'refs/heads/main') {
+    if($DryRun) {
+        npx semantic-release --dry-run
+    }
+    else {
+        npx semantic-release
+    }
+    
+    Write-Output "SEMANTIC_VERSION=$(Get-Content -Path 'SEMANTIC-VERSION.txt')" >> $env:GITHUB_OUTPUT
 }
 else {
-    npx semantic-release
+    Write-Output "SEMANTIC_VERSION=0.0.1" >> $env:GITHUB_OUTPUT
 }
-
-Write-Output "SEMANTIC_VERSION=$(Get-Content -Path 'SEMANTIC-VERSION.txt')" >> $env:GITHUB_OUTPUT
